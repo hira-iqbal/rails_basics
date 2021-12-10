481239a735ad1434fac604ba2e18e394f418ae30
Hira Iqbal <hirach502@gmail.com>
2021-12-10 16:19:43 +0500

    cleanup

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,15 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   after_create :assign_default_role
+  validate :must_have_a_role, on: :update
 
   def assign_default_role
     self.add_role(:newuser) if self.roles.blank?
+  end
+
+  def must_have_a_role
+    unless roles.any?
+      errors.add(:roles,"must have atleast one role")
+    end
   end
 end
